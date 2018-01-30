4d0923a70b62bb2981b09dc1ecee3d428bc6db3e
KY <infcurious@gmail.com>
2018-01-30 20:48:28 +0800

    Impl. autofix for spread-props rule

--- tests/lib/rules/spread-props.js
+++ tests/lib/rules/spread-props.js
@@ -38,18 +38,80 @@ ruleTester.run("spread-props", rule, {
     },
     {
       code: "<div foo={()=>{}}></div>"
+    },
+    {
+      code: "<div foo={this.foo}></div>"
+    },
+    {
+      code: "<div foo={foo()}></div>"
     }
   ],
 
   invalid: [
     {
       code: "<div foo={foo}></div>",
+      output: "<div {...{ foo }}></div>",
       errors: [{
         message: `
 Use spread attributes when identifier for prop's name and value are the same.
 Instead of <div foo={foo}></div>, do <div {...{ foo }}></div>
 `,
-        type: "JSXAttribute"
+        type: "JSXOpeningElement"
+      }]
+    },
+    {
+      code: "<div foo={foo} bar={bar}></div>",
+      output: "<div {...{ bar, foo }}></div>",
+      errors: [{
+        message: `
+Use spread attributes when identifier for prop's name and value are the same.
+Instead of <div foo={foo}></div>, do <div {...{ foo }}></div>
+`,
+        type: "JSXOpeningElement"
+      }]
+    },
+    {
+      code: "<div foo={foo} {...{ bar }}></div>",
+      output: "<div {...{ bar, foo }}></div>",
+      errors: [{
+        message: `
+Use spread attributes when identifier for prop's name and value are the same.
+Instead of <div foo={foo}></div>, do <div {...{ foo }}></div>
+`,
+        type: "JSXOpeningElement"
+      }]
+    },
+    {
+      code: "<div {...{ bar }} foo={foo}></div>",
+      output: "<div {...{ bar, foo }}></div>",
+      errors: [{
+        message: `
+Use spread attributes when identifier for prop's name and value are the same.
+Instead of <div foo={foo}></div>, do <div {...{ foo }}></div>
+`,
+        type: "JSXOpeningElement"
+      }]
+    },
+    {
+      code: "<div foo={foo} {...{ bar }} baz={baz}></div>",
+      output: "<div {...{ bar, baz, foo }}></div>",
+      errors: [{
+        message: `
+Use spread attributes when identifier for prop's name and value are the same.
+Instead of <div foo={foo}></div>, do <div {...{ foo }}></div>
+`,
+        type: "JSXOpeningElement"
+      }]
+    },
+    {
+      code: "<div foobar={foobar} foo={foo} {...{ bar }} baz={baz} barbaz={barbaz}></div>",
+      output: "<div {...{ bar, barbaz, baz, foo, foobar }}></div>",
+      errors: [{
+        message: `
+Use spread attributes when identifier for prop's name and value are the same.
+Instead of <div foo={foo}></div>, do <div {...{ foo }}></div>
+`,
+        type: "JSXOpeningElement"
       }]
     }
   ]
